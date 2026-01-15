af46b9220ae207d18a9bb2e6c0d325775bcd603c
BaKaioh <126205359+BaKaioh@users.noreply.github.com>
2026-01-15 18:20:25 -0400

    function atletas

--- notas-atletas.js
+++ notas-atletas.js
@@ -1,27 +1,59 @@
-  let atletas = [
- {
-   nome: "Cesar Abascal",
-   notas: [10, 9.34, 8.42, 10, 7.88]
- },
- {
-   nome: "Fernando Puntel",
-   notas:  [8, 10, 10, 7, 9.33]
- },
- {
-   nome: "Daiane Jelinsky",
-   notas: [7, 10, 9.5, 9.5, 8]
- },
- {
-   nome: "Bruno Castro",
-   notas: [10, 10, 10, 9, 9.5]
- }
-]
+class Atleta {
+  constructor(nome, notas) {
+    this.nome = nome;
+    this.notas = notas;
+  }
+  obterMedia() {
+    let emOrdem = [];
+    if (this.notas) {
+      emOrdem = this.notas.sort(function (a, b) {
+        return a - b;
+      });
+    }
+    let notasFatiadas = emOrdem.slice(1, 4);
 
+    let soma = notasFatiadas.reduce(function (contador, nota) {
+      return contador + nota;
+    });
+    return soma / notasFatiadas.length;
+    // let media = soma / notasFatiadas.length; // retorna a média (soma divida pelo total de notas[3 após o slice])
+    // return media;
+  }
+  exibirDados() {
+    return `Atleta: ${this.nome} 
+Notas Obtidas: ${this.notas} 
+Média Válida: ${this.obterMedia()}`;
+  }
+  adicionarAtleta(atleta) {
+    this.atletas.push(atleta);
+  }
+}
 
+const atletas = [
+  {
+    nome: "Cesar Abascal",
+    notas: [10, 9.34, 8.42, 10, 7.88],
+  },
+  {
+    nome: "Fernando Puntel",
+    notas: [8, 10, 10, 7, 9.33],
+  },
+  {
+    nome: "Daiane Jelinsky",
+    notas: [7, 10, 9.5, 9.5, 8],
+  },
+  {
+    nome: "Bruno Castro",
+    notas: [10, 10, 10, 9, 9.5],
+  },
+];
 
-// obterMediaDeNotas(){
-//   constructor()
+atletas.forEach(function (atleta) {
+  let atletasObj = new Atleta(atleta.nome, atleta.notas);
+  console.log(atletasObj.exibirDados());
+});
+// let osmar = new Atleta("Osmar Lopez", [9, 9, 10, 8, 10]); //teste
+// osmar.exibirDados(); // OK - funcionando
+// osmar.exibirDados();
 
-//   let notasOrdenadas = [];
-//   if
-// }
+// console.log(atletasNew);
